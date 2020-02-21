e6fad6759553866169494b17ec1c9fa2998b76b6
Roman Azeem <romanazeem007@gmail.com>
2020-02-21 19:05:12 +0500

    Profiles Done

--- config/db.js
+++ config/db.js
@@ -4,13 +4,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    console.log(db);
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
     });
-
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
